8cc15b909d2b5865808086955e7ad40ce828f554
David Li <li.davidm96@gmail.com>
2017-04-13 13:02:01 -0400

    Update Dockerfiles to handle new Trillian setup
    This does not create the log yet, though.

--- trillian/update-trillian.sh
+++ trillian/update-trillian.sh
@@ -12,9 +12,20 @@ cd $GOPATH/src/github.com/google/trillian
 go get -d -v -t ./...
 CGO_ENABLED=0 GOOS=linux go build ./...
 
+
+cd $GOPATH/src/github.com/google/trillian
+# Copy over the SQL initialization
+cp storage/mysql/storage.sql $COMPOSE_DIR/db/01-init.sql
+# Prepend a directive to use the appropriate database
+sed -i '1iUSE trillian;' $COMPOSE_DIR/db/01-init.sql
+
 cd $GOPATH/src/github.com/google/trillian
 cd examples/ct/ct_server
 CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o $COMPOSE_DIR/ct_server/main .
+
+cd $GOPATH/src/github.com/google/trillian
+cd server/trillian_log_signer
+CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o $COMPOSE_DIR/log_signer/main .
 
 cd $GOPATH/src/github.com/google/trillian
 cd server/trillian_log_server
@@ -23,4 +34,5 @@ CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o $COMPOSE_DIR/log_s
 cd $COMPOSE_DIR/trampoline
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o trampoline trampoline.go
 cp trampoline $COMPOSE_DIR/ct_server
+cp trampoline $COMPOSE_DIR/log_signer
 cp trampoline $COMPOSE_DIR/log_server
